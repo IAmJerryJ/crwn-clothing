17451aee479c3fa99052fbfe6aa08f1f9c043189
IAmJerryJ <jerryjia1999429@gmail.com>
2022-09-11 19:55:27 +1000

    fixed a bug in cart-dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,7 +3,7 @@ import './cart-item.styles.scss';
 const CartItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
     return (
-        <div classname='cart-item-container'>
+        <div className='cart-item-container'>
             <img src={imageUrl} alt={`${name}`} />
             <div className='item-details'>
                 <span className='name'>{name}</span>
